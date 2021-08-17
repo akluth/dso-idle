9325651cb039548cf922ea080ce009457619a01d
Alexander Kluth <alex@quackiquacki.org>
2021-08-17 21:20:17 +0200

    check if the window is closed and dos2unix
    * check if the target windows is closed to exit the program afterwards
      (when running as a standalone .exe)
    * dos2unix because duh
    
    Signed-off-by: Alexander Kluth <alex@quackiquacki.org>

--- dso-idle.py
+++ dso-idle.py
@@ -3,29 +3,36 @@ import pygetwindow
 import time
 import random
 import os
+import sys
 
 random.seed(os.urandom(256))
 
-dsoWindow = pygetwindow.getWindowsWithTitle("Drakensang Online | Das kostenlos spielbare Action-MMORPG - DSO")[0]
+dsoWindowTitle = "Drakensang Online | Das kostenlos spielbare Action-MMORPG - DSO"
+dsoWindow = pygetwindow.getWindowsWithTitle(dsoWindowTitle)[0]
 dsoWindow.maximize()
 dsoWindow.activate()
 
 screenWidth, screenHeight = pyautogui.size()
-currentMouseX, currentMouseY = pyautogui.position()
-
-thirdOfScreenWidth = screenWidth / 3
+thirdOfScreenWidth = int(screenWidth / 3)
+halfOfScreenHeight = int(screenHeight / 2)
 
 while True:
     pyautogui.moveTo(
-        screenWidth - (thirdOfScreenWidth),
-        screenHeight - (screenHeight / 2)
+        screenWidth - thirdOfScreenWidth,
+        screenHeight - halfOfScreenHeight
     )
     pyautogui.click()
     time.sleep(random.randint(1, 60))
 
     pyautogui.moveTo(
-        screenWidth - (thirdOfScreenWidth * 2),
-        screenHeight - (screenHeight / 2)
+        screenWidth - thirdOfScreenWidth * 2,
+        screenHeight - halfOfScreenHeight
     )
     pyautogui.click()
     time.sleep(random.randint(1, 60))
+
+    # ugh, hack, needed to determine if the window is still active. if it isn't, close the program gracefully
+    try:
+        pygetwindow.getWindowsWithTitle(dsoWindowTitle)[0]
+    except IndexError:
+        sys.exit(0)
